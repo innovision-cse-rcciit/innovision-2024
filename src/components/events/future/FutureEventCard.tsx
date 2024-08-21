a7439f2617572ad2a36540289ded45a8ad88f360
Dibs07 <banerjeedibs@gmail.com>
2024-08-22 01:40:58 +0530

    modified:   src/components/events/future/FutureEventCard.css 	modified:   src/components/events/future/FutureEventCard.tsx

--- src/components/events/future/FutureEventCard.tsx
+++ src/components/events/future/FutureEventCard.tsx
@@ -5,11 +5,16 @@ const FutureEventCard = ({ date, title }:{
     date: string;
     title: string;
 }) => {
+    const [isHovered, setIsHovered] = React.useState(false);
     return (
-        <div className="relative border border-black p-4 w-full max-w-xs mx-auto future-event-card">
+        <div onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)} className="relative border border-black p-4 w-full max-w-xs mx-auto future-event-card">
             <div className="absolute top-2 left-4 text-base font-Chakra_Petch font-semibold">{title}</div>
             <div className="absolute top-3 right-2 text-xs font-Chakra_Petch font-semibold">{date}</div>
-            <div className="mt-8 w-full h-40 bg-black transition-transform duration-300 ease-in-out transform hover:scale-100"></div>
+            <div className="relative mt-8 w-full h-40 bg-black transition-transform duration-300 ease-in-out transform hover:scale-100">
+                { isHovered && <h1 className="absolute top-0 left-0 text-white">
+                    Hello
+                </h1>}
+            </div>
         </div>
     );
 };
