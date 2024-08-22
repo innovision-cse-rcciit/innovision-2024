1dcaca46895a3be791b108b905d4b6ca91c43bee
Dibs07 <banerjeedibs@gmail.com>
2024-08-23 00:12:49 +0530

    new file:   src/app/events/page.tsx 	modified:   src/app/page.tsx 	deleted:    src/components/events/Events.tsx 	modified:   src/components/events/future/FutureEventCard.css 	modified:   src/components/events/future/FutureEventCard.tsx 	deleted:    src/components/events/future/FutureEventCards.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client"
-import Events from "@/components/events/Events";
 import { login } from "@/utils/functions/login";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-12 ">
-      <Events/>
+      
     </main>
   );
 }
