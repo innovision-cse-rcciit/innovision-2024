0e9c1c859d91a83722a39dcb3bfa5aa7cfe513e9
Subhadeep-coder <dharsubhadeep5@gmail.com>
2024-08-27 14:39:27 +0530

    Implemented Admin Dashboard

---
+++ actions/admin/add-event.ts
@@ -0,0 +1,47 @@
+import { Role } from '@/lib/schema/add-coordinator-volunteer-schema';
+import { supabase } from '@/lib/supabase-client';
+export const addEvent = async (values: any, roles: any) => {
+    console.log(values);
+    delete values.coordinator;
+    const { data, error } = await supabase.auth.getSession();
+
+    if (error) {
+        throw error;
+    }
+    const { data: eventInsertData, error: eventInsertError } = await supabase
+        .from("events")
+        .insert({
+            event_name: values.event_name,
+            event_mode: values.event_type,
+            banner_url: values.image_path,
+            description: values.description,
+            max_team_size: values.max_team_size,
+            min_team_size: values.min_team_size,
+            schedule: values.schedule,
+            rules: values.rules
+        });
+
+    if (!eventInsertData && eventInsertError)
+        throw eventInsertError;
+    console.log('Events Inserted');
+
+    // Use Promise.all to insert all roles concurrently
+    const insertRolePromises = roles.map((role: any) => {
+        return supabase.from("roles").insert({
+            id: data.session?.user.id,
+            role: role.role as Role,
+            event_id: eventInsertData.id // Use the inserted event's ID
+        });
+    });
+    console.log('Roles Insertion started');
+
+    const roleInsertResults = await Promise.all(insertRolePromises);
+    console.log('Roles Inserted');
+
+    // Check for errors in role inserts
+    roleInsertResults.forEach(({ error: roleInsertError }) => {
+        if (roleInsertError) {
+            throw roleInsertError;
+        }
+    });
+}
---
+++ src/components/admin/AddCoordinatorForm.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import React, { useState } from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+
+} from '../ui/dialog'
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import {
+    Role
+
+} from '@/lib/schema/add-coordinator-volunteer-schema';
+import { Button } from '../ui/button';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+
+} from '../ui/select';
+
+type Props = {
+    children: React.ReactNode;
+    event_name: string;
+    isOpen: boolean;
+    onClose: (isOpen: boolean) => void;
+    coorinatorValue: { name: string; email: string, role: Role }[];
+    setCoordinatorValue: (coordinator: { name: string; email: string, role: Role }[]) => void;
+}
+
+const AddCoordinatorForm = ({ children, event_name, isOpen, onClose, coorinatorValue, setCoordinatorValue }: Props) => {
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [coRole, setCoRole] = useState<Role>(Role.COORDINATOR);
+
+    const addCoordinator = (e: any) => {
+        setLoading(true);
+        const newCoordinator = { name, email, role: coRole };
+        // coorinatorValue.push(newCoordinator);
+        // setCoordinatorValue(coorinatorValue);
+        setCoordinatorValue(prevCoordinators => [...prevCoordinators, newCoordinator]);
+        setLoading(false);
+        setName("");
+        setEmail("");
+        onClose(!isOpen);
+    }
+
+    const role = coRole[0] + coRole.toLowerCase().substring(1);
+
+    return (
+        <Dialog
+            open={isOpen}
+            onOpenChange={onClose}
+        >
+            <DialogTrigger asChild>
+                {children}
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Add {role}</DialogTitle>
+                    <DialogDescription>Select {role} for {event_name === "" ? 'event' : event_name}</DialogDescription>
+                </DialogHeader>
+                <div className="flex flex-col gap-y-4">
+                    <Label>{role} Name</Label>
+                    <Input
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder={`${role} name`}
+                        disabled={loading}
+                    />
+
+                    <Label>{role} Email</Label>
+                    <Input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder={`${role} email`}
+                        disabled={loading}
+                    />
+
+                    <Label>Role</Label>
+                    <Select
+                        disabled={loading}
+                        onValueChange={(value: Role) => setCoRole(value)}
+                        defaultValue={coRole}
+                    >
+                        <SelectTrigger className="bg-zinc-300/50 border-0 text-black focus:ring-0 ring-offset-0 capitalize outline-none focus:ring-offset-0">
+                            <SelectValue placeholder="Select a role type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {
+                                Object.values(Role).map((channel) => (
+                                    <SelectItem
+                                        key={channel}
+                                        value={channel}
+                                        className="capitalize"
+                                    >
+                                        {channel.toLowerCase()}
+                                    </SelectItem>
+                                ))
+                            }
+                        </SelectContent>
+                    </Select>
+                </div>
+
+                <Button
+                    type="button"
+                    onClick={addCoordinator}
+                >
+                    Add {role}
+                </Button>
+                <DialogFooter>
+
+                </DialogFooter>
+            </DialogContent>
+        </Dialog >
+    )
+}
+
+export default AddCoordinatorForm
