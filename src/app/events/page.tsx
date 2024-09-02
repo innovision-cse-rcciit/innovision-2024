80a6e536935eeffd158a52a742ee759e95b52f5d
PratyushPal29 <29palpratyush04@gmail.com>
2024-09-03 01:39:59 +0530

    new file:   public/events/Events_bg_all.png 	new file:   public/events/Webify_poster.jpeg 	modified:   src/app/events/page.tsx 	modified:   src/components/events/future/EventGlowFont.css 	new file:   src/components/events/future/EventHoverEffect.css 	modified:   src/components/events/future/FutureEventCard.css 	modified:   src/components/events/future/FutureEventCard.tsx 	modified:   src/components/events/future/FutureEventsHeading.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,27 +4,24 @@ import FutureEventsHeading from '@/components/events/future/FutureEventsHeading'
 import React from 'react'
 
 const Event = () => {
-    const cardsData = [
-        { date: '02.09.2024', title: 'WEBIFY' },
-        { date: '03.09.2024', title: 'CODATHON' },
-        { date: '04.09.2024', title: 'HACKATHON' },
-        { date: '04.09.2024', title: 'BAG' },
-      ];
-    
+  const cardsData = [
+    { date: '02.09.2024', title: 'WEBIFY' },
+    { date: '03.09.2024', title: 'CODATHON' },
+    { date: '04.09.2024', title: 'HACKATHON' },
+    { date: '04.09.2024', title: 'BAG' },
+  ];
+
   return (
     <>
-   <div className="mx-auto min-h-screen max-w-full md:px-20 bg-black">
-   <div className="relative w-full ">
-      <div className="mx-auto max-w-full flex flex-col gap-10">
+      <div className="flex flex-col items-center py-4 bg-black">
+        {/* <img src='/events/Events_bg_all.png' className='h-screen w-screen object-cover' /> */}
         <FutureEventsHeading />
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-16 px-4">
           {cardsData.map((card, index) => (
             <FutureEventCard key={index} date={card.date} title={card.title} />
           ))}
         </div>
       </div>
-    </div>
-    </div>
     </>
   )
 }
--- src/components/events/future/FutureEventCard.tsx
+++ src/components/events/future/FutureEventCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './FutureEventCard.css';
+import './EventHoverEffect.css';
 
 const FutureEventCard = ({ date, title }: {
     date: string;
@@ -8,31 +9,18 @@ const FutureEventCard = ({ date, title }: {
     const [isHovered, setIsHovered] = React.useState(false);
     return (
         <div
+            id='cardContainer'
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-            className={`relative border border-black p-8 w-full max-w-xs mx-auto future-event-card ${isHovered ? 'hovered' : ''}`}
+            className={`relative border-4 border-[#B61B69] max-w-xs mx-auto rounded-xl`}
         >
-            <div className={`absolute top-2 left-4 text-base font-Chakra_Petch font-semibold ${isHovered ? 'hovered-text' : ''}`}>
-                {title}
-            </div>
-            <div className={`absolute top-3 right-4 text-xs font-Chakra_Petch font-semibold ${isHovered ? 'hovered-text' : ''}`}>
-                {date}
-            </div>
-            <div className={`relative mt-8 w-full h-48 bg-black transition-transform duration-300 ease-in-out transform ${isHovered ? 'scale-100' : ''}`}>
-                {isHovered && (
-                    <div className="flex flex-col items-start w-full">
-                        <ul className="text-xs space-y-2 mt-8">
-                            <li className="text-xs ml-2 cursor-pointer text-white font-Chakra_Petch font-semibold">RULES</li>
-                            <li className="text-xs ml-2 cursor-pointer text-white font-Chakra_Petch font-semibold">TEAM MEMBERS</li>
-                            <li className="text-xs ml-2 cursor-pointer text-white font-Chakra_Petch font-semibold">COORDINATORS</li>
-                        </ul>
-                        <div className="flex justify-center w-full mt-4">
-                            <button className="bg-black text-white px-4 rounded shadow-lg box-shadow text-xs font-Chakra_Petch font-semibold hovered-text">
-                                    REGISTER
-                            </button>
-                        </div>
-                    </div>
-                )}
+            <img src='/events/Webify_poster.jpeg' className='rounded-lg' />
+            <div id='overlay'>
+                <span>
+                    <button className='text-[#B51C69] px-2 py-2 w-32 rounded-lg text-lg font-Chakra_Petch bg-white'>
+                        Register
+                    </button>
+                </span>
             </div>
         </div>
     );
