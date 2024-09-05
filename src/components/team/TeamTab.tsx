04cea00490c25e02dbaaf85c16ac604bd658454f
Dibs07 <banerjeedibs@gmail.com>
2024-09-06 04:43:30 +0530

    modified:   src/app/team/[category]/page.tsx 	deleted:    src/app/team/page.tsx 	modified:   src/components/team/TeamCard.tsx 	modified:   src/components/team/TeamTab.tsx 	modified:   src/components/team/TeamWrapper.tsx 	modified:   src/utils/constants/navRoutes.ts 	modified:   tailwind.config.ts

--- src/components/team/TeamTab.tsx
+++ src/components/team/TeamTab.tsx
@@ -17,7 +17,7 @@ const TeamTab = ({team}: Props) => {
   return (
     <Link href={team.path} className="cursor-pointer">
       <Button
-        className={`text-white px-5 md:px-7 lg:px-10 py-1 md:py-2 lg:py-3 font-retrolight font-semibold text-lg data-[state=active]:bg-[#B51C69] data-[state=active]:text-white hover:scale-95 hover:border-2 ${ pathname === team.path && 'border-yellow-300 border-2' }`}
+        className={`text-white px-5 md:px-7 lg:px-10 py-1 md:py-2 lg:py-3 font-Keania_One font-semibold text-lg data-[state=active]:bg-[#B51C69] data-[state=active]:text-white hover:scale-95 hover:border-2 ${ pathname === team.path && 'border-yellow-300 border-2' }`}
       >
         {team.category}
       </Button>
