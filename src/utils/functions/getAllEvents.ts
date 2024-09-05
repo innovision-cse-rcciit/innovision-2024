53ce88a4417f991379ff836ccd80ec7cade085ca
Soumyaraj Bag <soumyarajbag@gmail.com>
2024-09-06 04:37:15 +0530

    fixes

--- src/utils/functions/getAllEvents.ts
+++ src/utils/functions/getAllEvents.ts
@@ -3,20 +3,6 @@ import { EventMode } from "@/lib/schema/add-event-schema";
 
 export async function getAllEvents() {
     try {
-        const { data, error } = await supabase.auth.getSession();
-        if (!data.session)
-            throw new Error("User must be logged in");
-
-        const userRole = await supabase
-            .from("roles")
-            .select()
-            .eq("id", data.session.user.id);
-
-        if (!userRole?.data?.[0])
-            throw new Error("User role isn't available");
-
-        if (userRole.data[0].role !== 'ADMIN')
-            throw new Error("This user cannot insert new event");
 
         const eventdata = await supabase
             .from("events")
