240e1e61f4b1149d81dd01fe4bf0f8c5194e4ae5
Soumyaraj Bag <soumyarajbag@gmail.com>
2024-09-10 00:56:19 +0530

    fix:file-size

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,6 +1,14 @@
 import { uploadToGoogleDrive } from "@/utils/functions/wallPicUpload";
 import { NextRequest, NextResponse } from "next/server";
 
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb', // Set desired size limit
+    },
+  },
+};
 // Handle file upload directly
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
